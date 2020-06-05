4cfa230e6989bec2f8d3599c8d6c889b9979fb34
JIAYuqi1998 <2016301580278@whu.edu.cn>
2020-06-05 23:42:06 +0800

    hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -5,27 +5,23 @@ import static edu.princeton.cs.algs4.StdRandom.uniform;
 
 public class PercolationStats {
     private double[] threshold;
-    private Percolation[] gridArray;
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
-        gridArray = new Percolation[T];
         threshold = new double[T];
         for (int i = 0; i < T; i++) {
-            gridArray[i] = pf.make(N);
-            int thre = 0;
+            Percolation ipf = pf.make(N);
             while (true) {
                 int col = uniform(0, N);
                 int row = uniform(0, N);
-                gridArray[i].open(row, col);
-                thre += 1;
-                if (gridArray[i].percolates()) {
+                ipf.open(row, col);
+                if (ipf.percolates()) {
                     break;
                 }
             }
-            threshold[i] = thre;
+            threshold[i] = (1.0*ipf.numberOfOpenSites()) / N * N;
 
         }
         // generate random position; open the position; repeat until percolates.
@@ -52,7 +48,7 @@ public class PercolationStats {
         return mean() + 1.96 * stddev() / Math.sqrt(threshold.length);
     }
 
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         PercolationStats test = new PercolationStats(20, 10, new PercolationFactory());
         System.out.println(test.mean());
     }
