25bd3735da52b7731244a05250e35a60701a9c7b
JIAYuqi1998 <2016301580278@whu.edu.cn>
2020-06-11 15:24:55 +0800

    Submission of clab8

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -6,15 +8,48 @@ import java.util.ArrayList;
  * If a flight starts at the same time as a flight's end time, they are
  * considered to be in the air at the same time.
  */
-public class FlightSolver {
 
+/**
+ * Use two priority queue to store the flight. One is ordered by starting time,
+ *  the other is by landing time. Firstly, we record the starting time of the
+ *  flight which takes off first, and the first landing time. In the specified
+ *  timeline, if the next flight took off before the landing time, they are in
+ *  the same span, add the passengers number, and store the current sum to maxSum,
+ *  instead, if the flight took off after the first flight landing, which means there
+ *  is a flight landing in the time span, we remove the passengers of landing plane.
+ *  Iteratively, we could get the time span with the maximum passengers in air.
+ */
+public class FlightSolver {
+    int maxSum = 0;
+    Comparator<Flight> startTimeComparator = (i, j) -> i.startTime - j.startTime;
+    Comparator<Flight> endTimeComparator = (i, j) -> i.endTime - j.endTime;
+    PriorityQueue<Flight> minStart = new PriorityQueue<Flight>(startTimeComparator);
+    PriorityQueue<Flight> minEnd = new PriorityQueue<Flight>(endTimeComparator);
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        for (Flight i : flights) {
+            minStart.add(i);
+            minEnd.add(i);
+        }
+        int sum = 0;
+        while(!minStart.isEmpty()){
+            Flight takeoff = minStart.peek();
+            Flight landing = minEnd.peek();
+            if (takeoff.startTime <= landing.endTime) {
+                sum += takeoff.passengers;
+                minStart.poll();
+            }
+            else  {
+                sum -= landing.passengers;
+                minEnd.poll();
+            }
+            if (maxSum < sum) {
+                maxSum = sum;
+            }
+        }
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        return maxSum;
     }
 
 }
